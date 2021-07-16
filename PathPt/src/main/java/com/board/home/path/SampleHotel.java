886cb2a086546cc51c5802b98cb822412918730e
sunriseinfo <sunriseinfo@DESKTOP-Q964TDE>
2021-07-16 16:17:49 +0900

    commitpath create

---
+++ PathPt/src/main/java/com/board/home/path/SampleHotel.java
@@ -0,0 +1,17 @@
+package com.board.home.path;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+public class SampleHotel {
+	private Chef chef;
+	
+	public SampleHotel(Chef chef) {
+		this.chef=chef;
+	}
+}
